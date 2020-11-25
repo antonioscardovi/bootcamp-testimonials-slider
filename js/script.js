ac3272f8a88fb9ed816d4c5a9c89567c60b5c914
Antonio Scardovi <antonio.scardovi@gmail.com>
2020-11-25 23:34:19 +0100

    desktop design and animations done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-const buttonPrev = document.getElementsByClassName("");
-const buttonNext = document.getElementsByClassName("");
+const imageTanya = document.getElementsByClassName("tanya");
+const imageJohn = document.getElementsByClassName("john");
+const testTanya = document.getElementsByClassName("testimonial-1");
+const testJohn = document.getElementsByClassName("testimonial-2");
+const buttonPrev = document.getElementById("prev");
+const buttonNext = document.getElementById("next");
+
+buttonPrev.addEventListener("click", nextSlide);
+buttonNext.addEventListener("click", nextSlide);
+
+function nextSlide() {
+  let compStyles = window.getComputedStyle(testJohn[0]);
+  if (compStyles.getPropertyValue("display") === "none") {
+    imageJohn.item(0).style.display = "inline";
+    testJohn.item(0).style.display = "flex";
+    imageTanya.item(0).style.display = "none";
+    testTanya.item(0).style.display = "none";
+  } else {
+    imageTanya.item(0).style.display = "inline";
+    testTanya.item(0).style.display = "flex";
+    imageJohn.item(0).style.display = "none";
+    testJohn.item(0).style.display = "none";
+  }
+}
